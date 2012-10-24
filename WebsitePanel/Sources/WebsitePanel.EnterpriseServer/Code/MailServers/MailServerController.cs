1123d00691e832e89c051718086eb0f6d727bd9b
robvde <robvde@ROBVDE913.europe.corp.microsoft.com>
2012-10-25 00:40:38 +0400

    Creating a SmarterMail account for the first time does not create the MX

--- WebsitePanel/Sources/WebsitePanel.EnterpriseServer/Code/MailServers/MailServerController.cs
+++ WebsitePanel/Sources/WebsitePanel.EnterpriseServer/Code/MailServers/MailServerController.cs
@@ -1056,6 +1056,9 @@ namespace WebsitePanel.EnterpriseServer
 				{
 					domain.MailDomainId = itemId;
 					ServerController.UpdateDomain(domain);
+
+                    domain = ServerController.GetDomain(domain.DomainId);
+                    ServerController.AddServiceDNSRecords(domain.PackageId, ResourceGroups.Mail, domain, "");
 				}
 
 				// check if instant alias must be added
